e74f0c0ce8b4348f87670748f50cf614cd4d4310
Akbarshokh Sobirov <asobirov357@gmail.com>
2023-06-10 20:37:05 +0100

    feat(ed): minor changes

--- apps/excalidraw/src/pages/index.tsx
+++ apps/excalidraw/src/pages/index.tsx
@@ -1,31 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import {
-  Excalidraw,
-  Footer,
-  LiveCollaborationTrigger,
-  MIME_TYPES,
-  MainMenu,
-  Sidebar,
-  exportToBlob,
-  exportToCanvas,
-  exportToClipboard,
-  exportToSvg,
-  restoreElements,
-  sceneCoordsToViewportCoords,
-  useHandleLibrary,
-  viewportCoordsToSceneCoords,
-} from "@excalidraw/excalidraw";
-import {
-  AppState,
-  BinaryFileData,
-  ExcalidrawImperativeAPI,
-  ExcalidrawInitialDataState,
-  PointerDownState as ExcalidrawPointerDownState,
-  Gesture,
-  LibraryItems,
-} from "@excalidraw/excalidraw/types/types";
 
 const DynamicExcalidraw = dynamic(
   () => import("@excalidraw/excalidraw").then((mod) => mod.Excalidraw),
@@ -38,7 +13,8 @@ const Home: NextPage = () => {
   return (
     <>
       <main className="flex h-screen flex-col items-center">
-        <DynamicExcalidraw />
+        <DynamicExcalidraw 
+        />
       </main>
     </>
   );
