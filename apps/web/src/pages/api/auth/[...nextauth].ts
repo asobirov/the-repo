f60b53685548453edaa68027f81ed7f3fd2967e5
Akbarshokh Sobirov <asobirov357@gmail.com>
2023-09-23 16:40:37 +0100

    feat: add main app as a submodule

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++
@@ -1,4 +0,0 @@
-import { authOptions } from "@tr/auth";
-import NextAuth from "next-auth";
-
-export default NextAuth(authOptions);
--- apps/web/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import baseConfig from "@tr/tailwind-config";
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.tsx"],
-  presets: [baseConfig],
-} satisfies Config;
