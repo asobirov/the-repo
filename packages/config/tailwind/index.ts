08f77682364162dfe3d2f19be02787b8894784ee
Akbarshokh Sobirov <asobirov357@gmail.com>
2023-09-30 02:16:52 +0100

    update border radius sizes

--- packages/config/tailwind/index.ts
+++ packages/config/tailwind/index.ts
@@ -49,6 +49,9 @@ export default {
         },
       },
       borderRadius: {
+        "3xl": "calc(var(--radius) + 6px)",
+        "2xl": "calc(var(--radius) + 4px)",
+        xl: "calc(var(--radius) + 2px)",
         lg: `var(--radius)`,
         md: `calc(var(--radius) - 2px)`,
         sm: "calc(var(--radius) - 4px)",
