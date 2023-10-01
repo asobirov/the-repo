615b1887069c3dde60383fc8a1e60d35da25893b
Akbarshokh Sobirov <asobirov357@gmail.com>
2023-10-01 06:50:44 +0100

    update styles + @tr/portfolio

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -22,8 +22,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        sm: "h-8 rounded-xl px-3 text-xs",
+        lg: "h-10 rounded-2xl px-8",
         icon: "h-9 w-9",
       },
     },
